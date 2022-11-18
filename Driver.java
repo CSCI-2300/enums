419681afc375612d562d317382b568192db97fec
kate-holdener <42073871+kate-holdener@users.noreply.github.com>
2022-11-18 10:45:37 -0600

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,24 @@
+public class Driver
+{
+   enum Month{
+     JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC;
+     int days;
+     public void setDays(int numDays){days = numDays;}
+   }
+
+   public static void main(String []args)
+   {
+      Month jan = Month.JAN;
+      jan.setDays(31);
+   
+
+      Month feb = Month.FEB;
+      feb.setDays(28);
+
+      System.out.println("Days in Feb " + feb.days);
+      System.out.println("Days in Jan " + jan.days);
+
+      Month jan2 = Month.JAN;
+      System.out.println("Days in Jan 2 " + jan2.days);
+   }
+}
